1b5dcc41a6169112810fb490946d400570b722bb
MeganusWesker <mohitjassal157@gmail.com>
2023-03-01 17:20:05 +0530

    cors credentinal true setted

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({limit:"50mb",extended:true}))
 app.use(
     cors({
       origin:"*",
+      credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
