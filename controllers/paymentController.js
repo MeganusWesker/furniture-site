53058702206b66cd4d20a14c0e833061d0e6f828
MeganusWesker <mohitjassal157@gmail.com>
2023-03-02 14:29:15 +0530

    chelga eska baap bhi

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,6 +5,8 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
 
   console.log(req.body);
+  console.log(process.env.STRIPE_SECRET_KEY);
+  
   const myPayment = await stripe.paymentIntents.create({
     amount: req.body.amount,
     currency: "inr",
